54118c8658a2bf8913669d26524338e3aed7afd0
Ronaldo Santana <ronaldosantana483@gmail.com>
2020-09-21 20:21:40 -0300

    Updates

---
+++ precache-manifest.af25dbb9d8804d157c33971b2aa024cd.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa06930b545bb407232e9c156e48eada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c53f93776276bf4b8037",
+    "url": "/static/css/main.d40ca035.chunk.css"
+  },
+  {
+    "revision": "ea8bca08429926eebfa4",
+    "url": "/static/js/2.2a4e93e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a4e93e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53f93776276bf4b8037",
+    "url": "/static/js/main.6fbc6244.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "021b9945a4e4e39ac7e7e6b95afc573b",
+    "url": "/static/media/bootstrap.021b9945.svg"
+  },
+  {
+    "revision": "d0cda54827bce47c3b4994058cb83e7b",
+    "url": "/static/media/close.d0cda548.svg"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/static/media/css.2db97d69.svg"
+  },
+  {
+    "revision": "4dc9f9cecb02dfad38ba3a095fd844e8",
+    "url": "/static/media/curriculo.4dc9f9ce.pdf"
+  },
+  {
+    "revision": "6faeb87f1aba58bb9eaccabd1137018a",
+    "url": "/static/media/figma.6faeb87f.svg"
+  },
+  {
+    "revision": "774c737a4f9cd77b81c99d330a7d813b",
+    "url": "/static/media/foto.774c737a.png"
+  },
+  {
+    "revision": "f90ecbc099704162f388f5bbe3e58823",
+    "url": "/static/media/git.f90ecbc0.svg"
+  },
+  {
+    "revision": "6196bae123f7cbfa45372535c5ad2703",
+    "url": "/static/media/html5.6196bae1.svg"
+  },
+  {
+    "revision": "f2bd0c0cb064f241f93ab02be9e524b0",
+    "url": "/static/media/javascript.f2bd0c0c.svg"
+  },
+  {
+    "revision": "5d2335924d8757727715893a58704d44",
+    "url": "/static/media/menu.5d233592.svg"
+  },
+  {
+    "revision": "1a78462c13429e10f1d5160064cbed0a",
+    "url": "/static/media/notebook.1a78462c.png"
+  },
+  {
+    "revision": "367504738b4d1b560819984f0e4310e3",
+    "url": "/static/media/npm.36750473.svg"
+  },
+  {
+    "revision": "69c2c2f02bf78695002380c98ed681e3",
+    "url": "/static/media/pattern.69c2c2f0.svg"
+  },
+  {
+    "revision": "37accd6c06268e901a2842ed52d6a29d",
+    "url": "/static/media/portfoliosite.37accd6c.png"
+  },
+  {
+    "revision": "19bf974b1cadc7b47c321a68c3013cef",
+    "url": "/static/media/reactjs.19bf974b.svg"
+  },
+  {
+    "revision": "04b946eddb180fa73e4bd46fe22965a7",
+    "url": "/static/media/scalenesite.04b946ed.png"
+  },
+  {
+    "revision": "e1ce9688e681d0be35856681141de731",
+    "url": "/static/media/ux-ux.e1ce9688.svg"
+  }
+]);
